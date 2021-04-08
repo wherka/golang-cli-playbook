55b1c265626ac3421e4a48a337f350dee9581fdf
Waldek HERKA <waldek.herka@amadeus.com>
2021-04-08 21:10:43 +0000

    Completed module

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -6,6 +6,7 @@ import (
 )
 
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
+
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
